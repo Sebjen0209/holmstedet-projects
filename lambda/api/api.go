7354bb045281643949a2ea51cec494f6872f5951
Sebjen0209 <sebastianjensen0209@gmail.com>
2025-09-24 12:03:38 +0200

    finalized the API layer

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/google/uuid"
 )
 
 type ApiHandler struct {
@@ -62,5 +63,153 @@ func (api *ApiHandler) DoesProjectExist(request events.APIGatewayProxyRequest) (
 	return events.APIGatewayProxyResponse{
 		Body:       "Succes",
 		StatusCode: http.StatusAccepted,
+	}, nil
+}
+
+func (api *ApiHandler) CreateProject(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var input types.RegisterProject
+
+	err := json.Unmarshal([]byte(request.Body), &input)
+
+	// tjekker om er der andre fejl
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid Request" + err.Error(),
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	//tjeker om felterne er tomme
+	if input.Description == "" || input.Repo == "" || input.Title == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid request" + err.Error(),
+			StatusCode: http.StatusBadGateway,
+		}, err
+	}
+
+	//når vi har tjekket om felterne er udfyldte, så laver vi et project med felterne
+	project := types.Project{
+		ProjectID:   uuid.New().String(),
+		Title:       input.Title,
+		Description: input.Description,
+		Repo:        input.Repo,
+	}
+
+	//tjekker om der er fejl på database laget
+	err = api.dbStore.InsertProject(project)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Error creating project",
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+
+	body, _ := json.Marshal(project)
+	return events.APIGatewayProxyResponse{
+		Body:       string(body),
+		StatusCode: http.StatusAccepted,
+	}, nil
+}
+
+func (api *ApiHandler) GetProject(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	projectID := request.PathParameters["projectID"]
+
+	//tjekker om der er et projekt med det angivede projektID
+	if projectID == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Missing ProjectID",
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
+	//Tjekker om projektet er i databasen
+	project, err := api.dbStore.GetProject(projectID)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Project not found",
+			StatusCode: http.StatusNotFound,
+		}, err
+	}
+
+	body, _ := json.Marshal(project)
+	return events.APIGatewayProxyResponse{
+		Body:       string(body),
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (api *ApiHandler) EditProject(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	projectID := request.PathParameters["projectID"]
+
+	if projectID == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Missing ProjectID",
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
+	// Takes the request and converts it to a Go struct using Unmarshal so we can work with it
+	var input types.RegisterProject
+	err := json.Unmarshal([]byte(request.Body), &input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid Request" + err.Error(),
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	//tjeker om felterne er tomme
+	if input.Description == "" || input.Repo == "" || input.Title == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid request" + err.Error(),
+			StatusCode: http.StatusBadGateway,
+		}, err
+	}
+
+	updatedProject := types.Project{
+		ProjectID:   projectID,
+		Title:       input.Title,
+		Description: input.Description,
+		Repo:        input.Repo,
+	}
+
+	// Checks for any errors returned from the database when updating the project
+	project, err := api.dbStore.EditProject(projectID, updatedProject)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Error updating the project",
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+
+	body, _ := json.Marshal(project)
+	return events.APIGatewayProxyResponse{
+		Body:       string(body),
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (api *ApiHandler) DeleteProject(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	projectID := request.PathParameters["projecktID"]
+
+	if projectID == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "There is no project with such ID",
+			StatusCode: http.StatusBadGateway,
+		}, nil
+	}
+
+	err := api.dbStore.DeleteProject(projectID)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Error deleting the project",
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "Project got deleted",
+		StatusCode: http.StatusOK,
 	}, nil
 }
